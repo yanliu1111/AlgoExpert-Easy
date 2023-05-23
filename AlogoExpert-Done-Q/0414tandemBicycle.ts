2ae281f0a8c7ae7205ba658ece99819dd48d68ac
yanliu1111 <liuyan0504@gmail.com>
2023-05-23 13:02:15 -0600

    feat: I am so LAZY!

--- AlogoExpert-Done-Q/0414tandemBicycle.ts
+++ AlogoExpert-Done-Q/0414tandemBicycle.ts
@@ -1,7 +1,17 @@
+//greedy algorithm
+//TIME: O(nlogn) time for sorting,for fast and low speed =O(n) + O(n), if use popular sorting algorithm: merge sort, quick sort, heap sort, most time ending using quick sort.
+//n+n+nlgn=O(nlogn)
+//SPACE: O(1) constant space; if use merge sort, O(2n) = O(n)
 export function tandemBicycle(
   redShirtSpeeds: number[],
   blueShirtSpeeds: number[],
   fastest: boolean
 ) {
-  return -1;
+  let totalSpeed = 0;
+  redShirtSpeeds.sort((a, b) => a - b);
+  blueShirtSpeeds.sort((a, b) => (fastest ? b - a : a - b));
+
+  for (let i = 0; i < redShirtSpeeds.length; i++)
+    totalSpeed += Math.max(redShirtSpeeds[i], blueShirtSpeeds[i]);
+  return totalSpeed;
 }
---
+++ AlogoExpert-OthersLang/0523tandemBicycle.py
@@ -0,0 +1,22 @@
+def tandemBicycle (redShirtSpeeds, blueShirtSpeeds, fastest):
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+
+    if not fastest:
+        reverseArrayInPlace(redShirtSpeeds)
+
+    totalSpeed = 0
+    for idx in range(len(redShirtSpeeds)):
+        rider1 = redShirtSpeeds[idx]
+        rider2= blueShirtSpeeds[len(blueShirtSpeeds) - idx - 1]
+        totalSpeed += max(rider1, rider2)
+    return totalSpeed
+
+
+def reverseArrayInPlace (array):
+    start = 0
+    end = len(array) - 1
+    while start < end:
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
