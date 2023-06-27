5652ce9c14c48e89c515962813feb311ef1ba2b7
yanliu1111 <liuyan0504@gmail.com>
2023-06-26 23:32:08 -0600

    feat: update solution

--- AlogoExpert-OthersLang/0622findSmallestD.js
+++ AlogoExpert-OthersLang/0622findSmallestD.js
@@ -68,3 +68,61 @@ console.log(findSmallestDifferent([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 // working for negative numbers as well
 console.log(findSmallestDifferent([-2, -1, 1, 2, 3]));
 //{ group1: [ -2, 3 ], group2: [ -1, 1, 2 ], difference: 1 }
+
+console.log(findSmallestDifferent([1, 2, 4, 8]));
+//{ group1: [ 1, 8 ], group2: [ 2, 4 ], difference: 3 }
+
+//Best Solution Jun 26, 2023
+function findGroups(array) {
+  const sortedArray = array.sort((a, b) => a - b); // Sort the array in ascending order
+  const result = [];
+  let smallestDifference = Number.MAX_SAFE_INTEGER;
+
+  const generateCombinations = (currentIndex, group1, group2) => {
+    if (currentIndex === sortedArray.length) {
+      const difference = Math.abs(sum(group1) - sum(group2));
+      if (difference <= smallestDifference) {
+        if (difference < smallestDifference) {
+          result.length = 0; // Reset the result array if a smaller difference is found
+          smallestDifference = difference;
+        }
+        result.push({
+          group1: group1.slice(), // Store a copy of the current group1 in the result array
+          group2: group2.slice(),
+          difference,
+        });
+      }
+      return;
+    }
+
+    // Include the current element in group1
+    group1.push(sortedArray[currentIndex]);
+    generateCombinations(currentIndex + 1, group1, group2);
+    group1.pop();
+
+    // Include the current element in group2
+    group2.push(sortedArray[currentIndex]);
+    generateCombinations(currentIndex + 1, group1, group2);
+    group2.pop();
+  };
+
+  generateCombinations(0, [], []);
+  return result;
+}
+
+function sum(array) {
+  return array.reduce((total, num) => total + num, 0);
+}
+
+// Test cases
+// const array1 = [1, 1, 1, 1, 10];
+// console.log(findGroups(array1));
+
+// const array2 = [1, 2, 3, 4, 5];
+// console.log(findGroups(array2));
+
+// const array3 = [-2, -1, 1, 2, 3];
+// console.log(findGroups(array3));
+
+// const array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(findGroups(array4));
