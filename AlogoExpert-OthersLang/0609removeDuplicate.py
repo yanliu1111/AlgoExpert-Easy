00a55bef9704c4f8d9263443047692c02dd0b6c1
yanliu1111 <liuyan0504@gmail.com>
2023-07-02 23:12:32 -0600

    docs: more nodes, but I like ts method

--- AlogoExpert-OthersLang/0609removeDuplicate.py
+++ AlogoExpert-OthersLang/0609removeDuplicate.py
@@ -1,5 +1,6 @@
 # O(n) time | O(1) space
-
+# because we all have to do is look trough all of the nodes in our linked list. We only have to look at wach of them once. And we don't use any external space either.
+# Other requirement is the list in ascending order always.
 class LinkedList:
     def __init__(self, value):
         self.value = value
