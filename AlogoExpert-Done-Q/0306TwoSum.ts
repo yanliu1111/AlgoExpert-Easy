4fdfd802d4a4ff28d5713ef31ff5db57fe45968a
yanliu1111 <liuyan0504@gmail.com>
2024-10-28 08:04:55 +0800

    test

--- AlogoExpert-Done-Q/0306TwoSum.ts
+++ AlogoExpert-Done-Q/0306TwoSum.ts
@@ -1,7 +1,7 @@
 function twoNumberSum(array: number[], targetSum: number) {
   // Write your code here.
   const adjustedMap = new Map<number, number>();
-  // Map<number, number>() is a generic type, it takes two arguments, the first is the key type, the second is the value type. In this case, the key is a number, and the value is also a number. why <>? because it's a generic type, it's a type that can be used in different ways. In this case, we're using it to create a map that takes a number as a key and a number as a value.what is key? key is the index of the array, value is the value of the array.
+  // Map<number, number>() is a generic type, it takes two arguments, the first is the key type, the second is the value type. In this case, the key is a number, and the value is also a number. why <>? because it's a generic type, it's a type that can be used in different ways. In this case, we're using it to create a map that takes a number as a key and a number as a value.what is key? key is the index of the array, value is the value of the array. test
   for (let i = 0; i < array.length; i++) {
     adjustedMap.set(targetSum - array[i], i);
   }
