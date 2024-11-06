843eb5950cca4048a39156d584e566f885cb1b80
yanliu1111 <liuyan0504@gmail.com>
2024-11-07 06:31:18 +0800

    docs: learn review

--- AlogoExpert-Done-Q/0328nonConstructibleChange.ts
+++ AlogoExpert-Done-Q/0328nonConstructibleChange.ts
@@ -1,5 +1,6 @@
 // Non-Constructible Change
-
+// coin > currentMax + 1 means we cannot create the minmum amount
+// 0 (nlogn) time | constant space 0(1) space
 export function nonConstructibleChange(coins: number[]) {
   coins.sort((a: number, b: number) => a - b);
   let currentMax = 0;
