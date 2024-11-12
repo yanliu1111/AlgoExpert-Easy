9882a82718f8d1b60e224f33c95c4208c928ff95
yanliu1111 <liuyan0504@gmail.com>
2024-11-12 19:30:17 +0800

    docs: learn node deepth

--- AlogoExpert-Done-Q/0413NodeDepth.ts
+++ AlogoExpert-Done-Q/0413NodeDepth.ts
@@ -11,6 +11,12 @@
 //   );
 // }
 
+//20241112, this question iterative approach and how the recursive approach are gonne work.
+// recursive approach is nicer than iterative approach, because as with many binary tree questions,recursive is clean
+// you can write out the algorithm in a couple of lines. Even the iterative approach does work, recursive is better.
+// wheneverr recursive is call on a particular node, we're gonna sum of the depth of the node plus the return value
+// of calls to the recurive function on the two children nodes of the particular node. recursive function f(n, d) deepth of the nodes -> f(n, d) = d + f(l, d+1) + f(r, d+1) l means left, r means rigth.
+
 // time complexity: O(n), space complexity: O(h)
 export function nodeDepths(root: BinaryTree) {
   let result = 0;
