019f2aff83bbd674ce5de45ce6e747700c20a5f8
yanliu1111 <liuyan0504@gmail.com>
2024-12-17 08:26:23 +0800

    feat: practice greedy algorithm

--- AlogoExpert-Done-Q/0414tandemBicycle.ts
+++ AlogoExpert-Done-Q/0414tandemBicycle.ts
@@ -1,7 +1,9 @@
+// tandemBicycle 20241217
 //greedy algorithm
 //TIME: O(nlogn) time for sorting,for fast and low speed =O(n) + O(n), if use popular sorting algorithm: merge sort, quick sort, heap sort, most time ending using quick sort.
 //n+n+nlgn=O(nlogn)
 //SPACE: O(1) constant space; if use merge sort, O(2n) = O(n)
+//ask about build-in method, reverse the array
 export function tandemBicycle(
   redShirtSpeeds: number[],
   blueShirtSpeeds: number[],
